df562ddf2df53e0e2713f566aaf71dc1897b3ed3
Sapar-s <smagulsapar52@gmail.com>
2025-03-24 13:08:09 +0800

    sign up design

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { useState } from "react";
+import FirstStep from "./components/FirstStep";
+import { SecondStep } from "./components/SecondStep";
+
+const SignUpPage = () => {
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const FormSteps = [FirstStep, SecondStep][currentStep];
+  return (
+    <div>
+      <FormSteps />
+    </div>
+  );
+};
+
+export default SignUpPage;
