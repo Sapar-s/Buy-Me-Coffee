425ef0eb8a942b5f3533164b2dac1e4dd67b68b5
Sapar-s <smagulsapar52@gmail.com>
2025-04-09 12:46:44 +0800

    fix

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,5 @@
 import { runQuery } from "@/util/queryService";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function GET(): Promise<NextResponse> {
   try {
